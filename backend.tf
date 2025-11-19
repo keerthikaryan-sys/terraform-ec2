dcab1411ea987d1a40bc0012d8f29462027be22e
keerthikaryan-sys <keerthikaryan@gmail.com>
2025-11-19 18:52:19 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "mybucket-keerthika1"  # Replace with your S3 bucket name
     key            = "ec2/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "mytable"           # Locking table name
+    dynamodb_table = "mytable1"           # Locking table name
     encrypt        = true
   }
 }
