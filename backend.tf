1e7d83680efa65eb55565a463b6c13a3cf5db67a
keerthikaryan-sys <keerthikaryan@gmail.com>
2025-11-19 18:34:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mybucket-keerthika"  # Replace with your S3 bucket name
+    bucket         = "mybucket-keerthika1"  # Replace with your S3 bucket name
     key            = "ec2/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "mytable"           # Locking table name
